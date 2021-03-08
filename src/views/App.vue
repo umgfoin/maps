ebda0f3626e69593d41c165bdb60b08017b068b3
Arne Hamann <kontakt+github@arne.email>
2021-03-08 15:48:59 +0100

    Disable devices on custom maps
    Signed-off-by: Arne Hamann <kontakt+github@arne.email>

--- src/views/App.vue
+++ src/views/App.vue
@@ -1296,12 +1296,17 @@ export default {
 		},
 		// devices
 		onDevicesClicked() {
-			this.devicesEnabled = !this.devicesEnabled
-			// get devices if we don't have them yet
-			if (this.devicesEnabled && this.devices.length === 0) {
-				this.getDevices()
+		    if (this.myMapId) {
+				showInfo(t('maps', 'Devices are not yet available on custom Maps'))
+			} else {
+				this.devicesEnabled = !this.devicesEnabled
+				// get devices if we don't have them yet
+				if (this.devicesEnabled && this.devices.length === 0) {
+					this.getDevices()
+				}
+				optionsController.saveOptionValues({ devicesEnabled: this.devicesEnabled ? 'true' : 'false' })
 			}
-			optionsController.saveOptionValues({ devicesEnabled: this.devicesEnabled ? 'true' : 'false' })
+
 		},
 		getDevices() {
 			if (!this.devicesEnabled) {
@@ -1442,6 +1447,10 @@ export default {
 		},
 		loadMap(myMap) {
 			this.myMapId = myMap.id
+			// Disable devices for custom maps
+			if (this.myMapId) {
+				this.devicesEnabled = false
+			}
 			optionsController.myMapId = myMap.id
 			const that = this
 			optionsController.restoreOptions(function() {
