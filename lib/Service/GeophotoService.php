0600517748224cfe7943ff1da3a7af2c89934550
umgfoin <umgfoin@users.noreply.github.com>
2025-01-08 14:50:36 +0100

    Update GeophotoService.php: clearCache Always return success as clearing an empty cache/queue would falsely report an error.
    Signed-off-by: umgfoin <umgfoin@users.noreply.github.com>

--- lib/Service/GeophotoService.php
+++ lib/Service/GeophotoService.php
@@ -74,11 +74,11 @@ class GeophotoService {
 	 * @return bool
 	 */
 	public function clearCache(string $userId = ''): bool {
-		$a = $this->photosCache->clear($userId);
-		$b = $this->timeOrderedPointSetsCache->clear($userId);
-		$c = $this->backgroundJobCache->clear('recentlyAdded:'.$userId);
-		$d = $this->backgroundJobCache->clear('recentlyUpdated:'.$userId);
-		return $a and $b and $c and $d;
+		$this->photosCache->clear($userId);
+		$this->timeOrderedPointSetsCache->clear($userId);
+		$this->backgroundJobCache->clear('recentlyAdded:'.$userId);
+		$this->backgroundJobCache->clear('recentlyUpdated:'.$userId);
+		return true;
 	}
 
 	/**
